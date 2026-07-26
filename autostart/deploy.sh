e5a7fe0c9055db63180ca1a881a5117fb598313a
Sela-Vie <yongcogiulio125@gmail.com>
2026-07-26 22:57:34 +0800

    programatically creates the .desktop file to put in the autostart folder

--- autostart/deploy.sh
+++ autostart/deploy.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 set -e
 
-script_path="${BASH_SOURCE[0]}"
-script_dir=$(dirname -- "$script_path")
+# ===============================================
+# SETUP
+# ===============================================
 
-cp $script_dir/templates.desktop ~/.config/autostart
+absolute_file_path="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)/$(basename -- "${BASH_SOURCE[0]}")"
+absolute_folder_path_m0=$(dirname -- "$absolute_file_path")
+absolute_folder_path_m1="$(cd "$absolute_folder_path_m0/.." && pwd)"
+
+LOG="$absolute_file_path.log"
+exec 2>>"$LOG"
+
+# ===============================================
+# CUSTOM VARS
+# ===============================================
+
+SCRIPT_NAME="run.sh"
+script_dir="$absolute_folder_path_m0/$SCRIPT_NAME"
+
+file_template="$absolute_folder_path_m0/templates.desktop"
+file_output="$HOME/.config/autostart/templates.desktop"
+
+# ===============================================
+# SCRIPT
+# ===============================================
+
+mkdir -p "$HOME/.config/autostart"
+sed "s|Exec=.*|Exec=sh -c '$script_dir'|" $file_template > $file_output
